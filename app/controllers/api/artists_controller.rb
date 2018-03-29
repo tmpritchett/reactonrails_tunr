0330f430d31f2fbe5f899ffe7853a26cf89cb82b
Tanya M Pritchett <tanyampritchett@Tanyas-MacBook-Pro.local>
2018-03-29 14:42:01 -0400

    made chages to artist controller

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,44 +1,46 @@
 class Api::ArtistsController < ApplicationController
     def index
-        @artists = Artist.all 
-        render json: {
-        artists: @artists
-        }
+      @artists = Artist.all
+      render json: {
+        artists:  @artists
+      }
     end
-
+  
     def show
-        @artist = Artist.find(params[:id])
-        render json: {
-            artist: @artist
-        }
+      @artist = Artist.find(params[:id])
+      render json: {
+        artist: @artist
+      }
     end
-
+  
     def create
-        @artist = Artist.create(artist_params)
-        render json: {
-            artist: @artist
-        }
+      @artist = Artist.create(artist_params)
+      render json: {
+        artist: @artist
+      }
     end
+  
     def update
-        @artist = Artist.find(params[:id])
-        @artist.update!(artist_params)
-
-        render json: {
-            artist: @artist
-        }
+      @artist = Artist.find(params[:id])
+      @artist.update!(artist_params)
+      
+      render json: {
+        artist: @artist
+      }
     end
+  
     def destroy
-        Artist.find(params[:id]).destroy
-        render json: {
+      Artist.find(params[:id]).destroy
+  
+      render json: {
         message: "Artist successfully destroyed"
-    }
+      }
     end
-
+  
     private
-
+  
     def artist_params
-        params.require(:artist).permit(:name, :photo_url, :nationality)
-
-end
-
-end
+      params.require(:artist).permit(:name, :photo_url, :nationality)
+    end
+  end
+  
